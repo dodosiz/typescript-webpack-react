dee2513716916ff73021b99258c00689e7d02327
Theo Asvestopoulos <t.v.asvestopoulos@gmail.com>
2022-12-02 01:54:02 +0100

    implemented the app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div className="main">
+      <h1>Webpack is Working!</h1>
+      <p>Next step, modify the App component with your custom code.</p>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".ts", ".tsx", ".js", ".json"],
+        },
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
